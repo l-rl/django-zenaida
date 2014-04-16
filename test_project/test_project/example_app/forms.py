3b8cce7dc81ef1bc8054aaa65950b8e213c28be0
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-04-15 17:23:57 -0700

    Added some more fields to the example app forms.

--- test_project/test_project/example_app/forms.py
+++ test_project/test_project/example_app/forms.py
@@ -1,5 +1,17 @@
 import floppyforms as forms
 
 
+CHOICES = (
+    ('1', 'Choice 1'),
+    ('2', 'Choice 2'),
+    ('3', 'Choice 3'),
+)
+
+
 class Form1(forms.Form):
     char_field = forms.CharField()
+    boolean_field = forms.BooleanField()
+    choice_field = forms.ChoiceField(choices=CHOICES)
+    radio_select = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
+    multi_select = forms.MultipleChoiceField(choices=CHOICES)
+    checkbox_multiple_select = forms.MultipleChoiceField(choices=CHOICES, widget=forms.CheckboxSelectMultiple)
