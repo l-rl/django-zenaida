868c4643ce47d96bda6c76afd60ab39b7bb01cc1
Stephen Burrows <stephen.r.burrows@gmail.com>
2014-05-09 23:23:27 -0700

    Added filters for handling money display.

--- zenaida/templatetags/zenaida.py
+++ zenaida/templatetags/zenaida.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import decimal
+
 from django import template
 from django.forms.widgets import CheckboxInput
 
@@ -7,6 +9,29 @@ from django.forms.widgets import CheckboxInput
 register = template.Library()
 
 
+CURRENCY_TO_SYMBOL = {
+    'USD': ('$', 'left'),
+}
+
+
 @register.filter
 def is_checkbox(field):
     return isinstance(field.field.widget, CheckboxInput)
+
+
+@register.filter
+def format_money(amount, currency):
+    amount = decimal.Decimal(amount).quantize(decimal.Decimal('0.01'))
+    if currency in CURRENCY_TO_SYMBOL:
+        symbol, side = CURRENCY_TO_SYMBOL[currency]
+        format = u'{symbol}{amount}' if side == 'left' else '{amount}{symbol}'
+        return format.format(symbol=symbol, amount=amount)
+    return u'{} {}'.format(amount, currency)
+
+
+@register.filter
+def absolute_value(value):
+    try:
+        return abs(value)
+    except TypeError:
+        return ''
