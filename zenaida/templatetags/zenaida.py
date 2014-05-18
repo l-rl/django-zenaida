9db1bb7aa71ab7666a9810e27defffd473902988
Harris Lapiroff <harris@chromamine.com>
2014-05-17 22:02:31 -0400

    Couple timedelta filters.

--- zenaida/templatetags/zenaida.py
+++ zenaida/templatetags/zenaida.py
@@ -1,6 +1,8 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 
+import datetime
 import decimal
+import pytz
 
 from django import template
 from django.forms.widgets import CheckboxInput
@@ -35,3 +37,38 @@ def absolute_value(value):
         return abs(value)
     except TypeError:
         return ''
+
+
+@register.filter
+def td_timeuntil(value, now=None):
+    """
+    Takes a datetime value and returns the timedelta between it and now.
+    Optionally a second datetime value can be provided and it will return the
+    difference between those two values.
+
+    """
+
+    if not now:
+      now = datetime.datetime.now(pytz.utc)
+
+    return value - now
+
+
+@register.assignment_tag
+def td_to_dict(td):
+    """
+    Takes a timedelta and returns a dict with the following keys:
+
+    * days
+    * hours
+    * minutes
+    * seconds
+
+    """
+
+    return {
+      'days': td.days,
+      'hours': td.seconds // 3600,
+      'minutes': td.seconds // 60,
+      'seconds': td.seconds % 60,
+    }
